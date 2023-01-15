3348c98b8d607a1358030c9fa5367554a86bbacc
Małgorzata Banaszkiewicz <malnutelka@gmail.com>
2023-01-15 22:23:08 +0100

    some tasks from Kamil

---
+++ javascript-basics/jswithKamil.js
@@ -0,0 +1,30 @@
+function min(a, b) {
+let result = Math.min(a, b);
+return result;
+}
+
+console.log(min(2, 5));
+console.log(min(3, -1));
+console.log(min(1, 1));
+
+
+function pow(a, b) {
+    for (let i= ; i<b ; i++)
+result = result * b
+}
+
+function evenOrOdd(n) {
+    return n % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(31));
+
+function repeat(a, b) {
+    let laughter = " ";
+for ( let i = 0; i < b ; a++);
+laughter += "ha"
+return laughter;
+}
+
+console.log(repeat(3));
