dc91c987767b06f627b4ea71465909b25f959715
Natalia <n.ilczyna@gmail.com>
2023-01-15 17:50:10 +0100

    my_solutions

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -1,17 +1,81 @@
 // Zadanie 1.
-
+let a = "123";
+let b = 123;
+console.log(a == b);
+console.log(a === b);
 // Zadanie 2.
-
+let value = 0;
+let value2 = false;
+console.log(value == value2);
+console.log(value === value2);
 // Zadanie 3.
+let user = "Natalia";
+console.log(user);
 
+let age = 24;
+console.log(age);
+
+let city = "Lublin";
+console.log(city);
+
+let concatExample =
+  "Our user name is:" +
+  " " +
+  user +
+  " " +
+  "and is" +
+  " " +
+  age +
+  " " +
+  "years old and lives in" +
+  " " +
+  city;
+console.log(concatExample);
 // Zadanie 4.
 
+function sum(a, b) {
+  return a + b;
+}
+
+let ten = sum(5, 5);
+
+let four = sum(2, 2);
+
+let myAddedNumbers = sum(ten, four);
+
+console.log(myAddedNumbers);
+
 // Zadanie 5.
+let x = "userName";
+let y = "8";
+console.log(x);
+console.log(y);
+
+let concatxy = `Username:` + " " + x + " " + `have length:` + " " + y + ".";
+console.log(concatxy);
 
 // Zadanie 6.
+let firstUser = "Natalka";
+let secondUser = "Ola";
+
+if (firstUser === secondUser) {
+  console.log("O! The names are the same!");
+} else {
+  console.log("Unfortunately the names are different");
+}
 
 // Zadanie 7.
+let age1 = 24;
 
+if (age1 >= 18) {
+  console.log("User is adult");
+} else {
+  console.log("User is underage");
+}
+
+let age2 = 26;
+let adult = age2 >= 18 ? "User is adult" : "User is underage";
+console.log(adult);
 // Zadanie 8.
 
 // Zadanie 9.
