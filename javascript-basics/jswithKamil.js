691f844e4c4882c7d86666af9c2746145a939e74
Małgorzata Banaszkiewicz <malnutelka@gmail.com>
2023-01-17 10:14:30 +0100

    added some solutions

--- javascript-basics/jswithKamil.js
+++ javascript-basics/jswithKamil.js
@@ -1,17 +1,17 @@
-function min(a, b) {
-let result = Math.min(a, b);
-return result;
-}
+// function min(a, b) {
+// let result = Math.min(a, b);
+// return result;
+// }
 
-console.log(min(2, 5));
-console.log(min(3, -1));
-console.log(min(1, 1));
+// console.log(min(2, 5));
+// console.log(min(3, -1));
+// console.log(min(1, 1));
 
 
-function pow(a, b) {
-    for (let i= ; i<b ; i++)
-result = result * b
-}
+// function pow(a, b) {
+//     for (let i= ; i<b ; i++)
+// result = result * b
+// }
 
 function evenOrOdd(n) {
     return n % 2 === 0 ? 'even' : 'odd';
@@ -20,11 +20,12 @@ function evenOrOdd(n) {
 console.log(evenOrOdd(2));
 console.log(evenOrOdd(31));
 
-function repeat(a, b) {
+function repeat(b) {
     let laughter = " ";
-for ( let i = 0; i < b ; a++);
-laughter += "ha"
-return laughter;
+    for ( let i = 0; i < b ; i++) {
+        laughter += "ha";
+    }
+    return laughter;
 }
 
 console.log(repeat(3));
