39e490d6358de6a3fa241e0eb4406112e403a3bb
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-01-29 19:15:30 +0100

    Tasks 4-8

--- javascript-dom-and-forms/Mariusz_Przybylski/script.js
+++ javascript-dom-and-forms/Mariusz_Przybylski/script.js
@@ -2,7 +2,42 @@ console.log("DOM API");
 
 const header = document.getElementById("header");
 
-console.log(header);
+console.log(header.innerText);
 
 header.classList.add("header");
 header.innerText = "Jestem nagłówkiem";
+
+const listItems = ["a", "b", "c", "d"];
+
+const orderedList = document.createElement("ol");
+
+listItems.forEach((item) => {
+	const listItem = document.createElement("li");
+	listItem.innerText = item;
+	orderedList.append(listItem);
+});
+
+document.body.append(orderedList);
+
+const allListItems = document.querySelectorAll("li");
+
+allListItems.forEach((item) => item.classList.add("listItem"));
+
+header.classList.remove("header");
+header.remove();
+
+const button = document.createElement("button");
+
+button.innerText = "KLIKNIJ MNIE";
+
+document.body.append(button);
+
+const toggleBackgroundColor = () => {
+	if (!document.body.style.backgroundColor) {
+		document.body.style.backgroundColor = "#123123";
+	} else {
+		document.body.removeAttribute("style");
+	}
+};
+
+button.addEventListener("click", toggleBackgroundColor);
