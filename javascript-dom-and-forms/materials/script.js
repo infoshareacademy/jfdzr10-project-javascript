7481c1884a1ed2d03af9758f8d5b9697d9a58bb4
Adrian Kulesza <a.kulesza249@gmail.com>
2023-01-29 19:58:22 +0100

    finished 4 chapters and 5 chapter in work

--- javascript-dom-and-forms/materials/script.js
+++ javascript-dom-and-forms/materials/script.js
@@ -10,3 +10,15 @@ header.classList.add("header");
 header.innerText = "Jestem nagłówkiem";
 
 // 4
+const listItems = ["a", "b", "c", "d"];
+const list = document.createElement("ol");
+listItems.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.innerText = item;
+  list.appendChild(listItem);
+});
+document.body.appendChild(list);
+
+// 5
+const test = document.querySelectorAll("li");
+console.log(test);
