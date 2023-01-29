5ba6f88560c05c9a68c3a3b65176c6f935b2f4ca
Agata <agatka.fermatka@gmail.com>
2023-01-29 19:01:29 +0100

    exercises 0-3 done

--- javascript-dom-and-forms/materials/script.js
+++ javascript-dom-and-forms/materials/script.js
@@ -1,2 +1,10 @@
 console.log("DOM API");
-const document = document.querySelector("header");
+const header = document.getElementById("header");
+
+console.log(header);
+console.log(header.innerText);
+
+header.classList.add("header");
+console.log(header);
+
+header.innerText = "Jestem nagłówkiem";
