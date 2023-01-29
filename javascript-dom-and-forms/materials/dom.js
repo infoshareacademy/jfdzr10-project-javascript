3c07f8d82a3ee5f3587a12f8ee266917cf53eca8
Natalia <n.ilczyna@gmail.com>
2023-01-29 19:40:10 +0100

    Koniec zadania

--- javascript-dom-and-forms/materials/dom.js
+++ javascript-dom-and-forms/materials/dom.js
@@ -5,3 +5,19 @@ console.log(header);
 header.classList.add("header");
 
 document.querySelector("#header").innerText = 'Jestem nagłówkiem'
+//
+const listItems = ["a", "b", "c", "d"];
+
+const olList = document.createElement("ol");
+console.log("ol");
+
+listItems.forEach((element) => {
+    const newListItem = document.createElement("li");
+    newListItem.innerText = element;
+    olList.append(newListItem);
+});
+
+document.body.append(olList);
+console.log(listItems);
+//
+document.querySelector
