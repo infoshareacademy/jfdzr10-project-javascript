f79f33c9819ce77c5852e725b464bded7517839d
Avartil <33838459+Avartil@users.noreply.github.com>
2023-01-29 19:21:50 +0100

    counter

---
+++ javascript-dom-and-forms/zad2/app.js
@@ -0,0 +1,28 @@
+const bodyElement = document.querySelector("body");
+const buttonPlus = document.createElement("button");
+const counterViewer = document.createElement("input");
+const buttonMinus = document.createElement("button");
+counterViewer.setAttribute("id", "counter");
+counterViewer.setAttribute("disabled", true);
+buttonMinus.setAttribute("id", "minus");
+buttonPlus.setAttribute("id", "plus");
+
+buttonMinus.innerText = "-";
+buttonPlus.innerText = "+";
+
+let counter = 0;
+counterViewer.value = counter;
+
+bodyElement.append(buttonMinus);
+bodyElement.append(counterViewer);
+bodyElement.append(buttonPlus);
+
+buttonMinus.addEventListener("click", (e) => {
+  e.preventDefault();
+  counterViewer.value = counter--;
+});
+
+buttonPlus.addEventListener("click", (e) => {
+  e.preventDefault();
+  counterViewer.value = counter++;
+});
