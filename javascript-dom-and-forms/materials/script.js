a554c2b3005be41e62792e5a5c163c67ba926842
Mateusz Mudry <matmudry@gmail.com>
2023-01-29 19:11:20 +0100

    Header created in index.html

---
+++ javascript-dom-and-forms/materials/script.js
@@ -0,0 +1,9 @@
+console.log('DOM API');
+
+const header = document.getElementById('header');
+
+console.log(header);
+
+header.classList.add('header');
+
+document.querySelector('#header').innerHTML = 'Jestem nagłówkiem'
