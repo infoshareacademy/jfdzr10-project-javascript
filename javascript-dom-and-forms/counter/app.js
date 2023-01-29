19995d4d9d09b54f2c6c401f6da7dc750d56cbf6
Avartil <33838459+Avartil@users.noreply.github.com>
2023-01-29 20:33:50 +0100

    czyszczenie

--- javascript-dom-and-forms/counter/app.js
+++ javascript-dom-and-forms/counter/app.js
@@ -3,11 +3,7 @@ const buttonPlus = document.createElement("button");
 const counterViewer = document.createElement("input");
 const buttonMinus = document.createElement("button");
 
-counterViewer.setAttribute("id", "counter");
 counterViewer.setAttribute("disabled", true);
-
-buttonMinus.setAttribute("id", "minus");
-buttonPlus.setAttribute("id", "plus");
 
 buttonMinus.innerText = "-";
 buttonPlus.innerText = "+";
@@ -15,16 +11,10 @@ buttonPlus.innerText = "+";
 let counter = 0;
 counterViewer.value = counter;
 
-bodyElement.append(buttonPlus);
-bodyElement.append(counterViewer);
-bodyElement.append(buttonMinus);
+bodyElement.append(buttonPlus, counterViewer, buttonMinus);
 
-const minusOne = () => {
-  counterViewer.value = --counter;
-};
-const plusOne = () => {
-  counterViewer.value = ++counter;
-};
+const minusOne = () => (counterViewer.value = --counter);
+const plusOne = () => (counterViewer.value = ++counter);
 
 buttonMinus.addEventListener("click", minusOne);
 
