c0961c61a62dbe90f2f19077f16147a430793931
Adrian Kulesza <a.kulesza249@gmail.com>
2023-01-29 19:02:34 +0100

    finished 3 chapters

---
+++ javascript-dom-and-forms/materials/script.js
@@ -0,0 +1,12 @@
+// 1
+console.log("DOM API");
+const header = document.getElementById("header");
+console.log(header);
+
+// 2
+header.classList.add("header");
+
+// 3
+header.innerText = "Jestem nagłówkiem";
+
+// 4
