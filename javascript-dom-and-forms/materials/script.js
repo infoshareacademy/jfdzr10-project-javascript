ee0e5217242cb5f6b63d85680916b279275d54aa
Agata <agatka.fermatka@gmail.com>
2023-01-29 19:50:18 +0100

    exercises 1-7 done

--- javascript-dom-and-forms/materials/script.js
+++ javascript-dom-and-forms/materials/script.js
@@ -3,8 +3,35 @@ const header = document.getElementById("header");
 
 console.log(header);
 console.log(header.innerText);
-
+//2
 header.classList.add("header");
 console.log(header);
 
 header.innerText = "Jestem nagłówkiem";
+
+const listItems = ["A", "B", "C", "D"];
+
+const olList = document.createElement("ol");
+console.log("ol");
+// 4
+listItems.forEach((element) => {
+  const ilListItem = document.createElement("li");
+  ilListItem.innerText = element;
+  olList.append(ilListItem);
+});
+document.body.append(olList);
+
+console.log(listItems);
+
+// 5
+document.querySelectorAll("li").forEach((element) => {
+  element.classList.add("listItem");
+});
+
+//6
+header.classList.remove("header");
+
+//7
+header.remove();
+
+//8
