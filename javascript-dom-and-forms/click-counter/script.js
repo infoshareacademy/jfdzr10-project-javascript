2541d75c5220e9e4534270c803833b0a88483b5f
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-01-29 19:47:34 +0100

    Counter task

---
+++ javascript-dom-and-forms/click-counter/script.js
@@ -0,0 +1,36 @@
+const bodyTag = document.body;
+bodyTag.style.display = "flex";
+bodyTag.style.alignItems = "center";
+bodyTag.style.justifyContent = "center";
+bodyTag.style.height = "100vh";
+bodyTag.style.gap = "20px";
+
+const plusButton = document.createElement("button");
+plusButton.innerText = "+";
+
+const minusButon = document.createElement("button");
+minusButon.innerText = "-";
+
+const actualNumber = document.createElement("p");
+actualNumber.style.fontSize = "48px";
+
+let numberValue = 0;
+actualNumber.innerText = numberValue;
+
+const increaseNumber = () => {
+	numberValue++;
+	actualNumber.innerText = numberValue;
+};
+
+const decreaseNumber = () => {
+	numberValue--;
+	actualNumber.innerText = numberValue;
+};
+
+plusButton.addEventListener("click", increaseNumber);
+
+minusButon.addEventListener("click", decreaseNumber);
+
+bodyTag.append(plusButton);
+bodyTag.append(actualNumber);
+bodyTag.append(minusButon);
