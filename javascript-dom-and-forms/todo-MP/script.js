8ac81b739927472c2abb743f0f47ccbafe404005
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-01-30 10:56:11 +0100

    Create add to do handler with validation

--- javascript-dom-and-forms/todo-MP/script.js
+++ javascript-dom-and-forms/todo-MP/script.js
@@ -1,1 +1,44 @@
+const errorMessage = document.querySelector("span");
 
+const inputValidator = (content) => {
+	if (content) {
+		errorMessage.classList.add("hidden");
+		errorMessage.classList.remove("error");
+		return true;
+	}
+	errorMessage.classList.remove("hidden");
+	errorMessage.classList.add("error");
+	return false;
+};
+
+const createTodoItem = (todo) => {
+	const todoList = document.querySelector("#todoList");
+
+	const todoItem = document.createElement("li");
+	todoItem.setAttribute("id", todo.id);
+	todoItem.textContent = todo.value;
+
+	todoList.appendChild(todoItem);
+};
+
+const handleAddTodo = (e) => {
+	e.preventDefault();
+	const form = e.target;
+	const todoContent = form.todo.value;
+
+	const isValue = inputValidator(todoContent);
+
+	if (!isValue) {
+		return;
+	}
+
+	const todo = {
+		id: crypto.randomUUID(),
+		value: todoContent,
+	};
+
+	createTodoItem(todo);
+	form.reset();
+};
+
+document.querySelector("form").addEventListener("submit", handleAddTodo);
