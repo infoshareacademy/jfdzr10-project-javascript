7d2fc8e0aff4cb55696563b969e8b1077f66dc04
Wicki <mateuszwicki1@gmail.com>
2023-02-12 17:03:49 +0100

    task 3

--- javascript-promises/tasks/task-3-async-dom-api/main.js
+++ javascript-promises/tasks/task-3-async-dom-api/main.js
@@ -1,1 +1,24 @@
+const toastButton = document.querySelectorAll("[data-button]")[0];
 
+const toastArray = [];
+
+const renderToast = () => {
+  toastArray.forEach((el, idx) => {
+    el.style.top = `calc(100vh - ${idx * 150 + 150}px)`;
+    el.style.display = "flex";
+    document.body.appendChild(el);
+  });
+};
+
+toastButton.addEventListener("click", () => {
+  const toast = document.createElement("div");
+  toast.className = "toast-wrapper";
+  toast.innerText = "Infoshare Academy";
+  toastArray.push(toast);
+  setTimeout(() => {
+    const teostToRemove = toastArray.pop();
+    document.body.removeChild(teostToRemove);
+    renderToast();
+  }, 2000);
+  renderToast();
+});
