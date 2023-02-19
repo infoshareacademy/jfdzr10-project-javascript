b0e5a1ef21fbc0b6870127f63cb440317f44482f
Agata <agatka.fermatka@gmail.com>
2023-02-19 22:19:22 +0100

    data fetched, basic css done

--- javascript-fetch/newsolution/main.js
+++ javascript-fetch/newsolution/main.js
@@ -1,79 +1,28 @@
-const container = document.querySelector(".container");
+const pokemonGrid = document.querySelector(".pokemon-grid");
 
-fetch("https://pokeapi.co/api/v2/pokemon")
-  .then((response) => response.json())
-  .then((data) => {
-    const pokemon = data.results.map((result) => ({
-      name: result.name,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-        result.url.split("/")[6]
-      }.png`,
-    }));
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const data = await response.json();
+  const pokemon = data.results;
 
-    pokemon.forEach((p) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
+  pokemon.forEach((pokemon) => {
+    const pokemonElement = document.createElement("div");
+    pokemonElement.classList.add("pokemon");
 
-      const name = document.createElement("h2");
-      name.textContent = p.name;
+    const pokemonImage = document.createElement("img");
+    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      pokemon.url.split("/")[6]
+    }.png`;
 
-      const img = document.createElement("img");
-      img.src = p.image;
+    const pokemonName = document.createElement("div");
+    pokemonName.classList.add("pokemon-name");
+    pokemonName.innerText = pokemon.name;
 
-      card.appendChild(name);
-      card.appendChild(img);
-      container.appendChild(card);
-    });
+    pokemonElement.appendChild(pokemonImage);
+    pokemonElement.appendChild(pokemonName);
+
+    pokemonGrid.appendChild(pokemonElement);
   });
+}
 
-// const main = document.querySelector("main");
-// const url = "https://pokeapi.co/api/v2/pokemon";
-
-// // let data;
-
-// const createPokemon = (pokemon) => {
-//   const pokemonCard = document.createElement("div");
-//   pokemonCard.classList.add("card");
-//   document.body.appendChild([pokemonCard]);
-
-//   const pokemonImg = document.createElement("div");
-//   pokemonImg.src = pokemon.sprites.front_default;
-//   document.body.appendChild([pokemonImg]);
-// };
-
-// async function fetchData(id) {
-//   const response = await fetch("$url/${id}/");
-//   const data = await response.json();
-//   createPokemon(data);
-// }
-
-// const fetchPokemons = (amountOfPokemons) => {
-//   for (let i = 1; i < amountOfPokemons; i++) {
-//     fetchData(i);
-//   }
-// };
-
-// fetchPokemons(1);
-
-//     console.log(data);
-//     data.map((item) => {
-//       item.name;
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// function name(params) {
-//   data;
-// }
-
-// const tes = () => {
-//   fetch("url")
-//     .then((response) => response.json())
-//     .then((pokemon) => {
-//       createPokemon(pokemon);
-//     });
-// };
-
-// tes();
+fetchPokemon();
